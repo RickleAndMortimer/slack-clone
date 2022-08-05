46f66411d115dc66cfa3c66dfe7d110255c331ef
nxya <nathacutlan@gmail.com>
2022-08-05 17:50:10 -0400

    usernames

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var socket = io();
+let socket = io();
 
-var messages = document.getElementById('messages');
-var form = document.getElementById('form');
-var input = document.getElementById('input');
+let messages = document.getElementById('messages');
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+
+let user = document.getElementById('username');
+let signin = document.getElementById('signin');
+
+let username = '';
 
 function makeMessageHTML(username, msg) {
-    var item = document.createElement('li');
-    var time = new Date();
+    let item = document.createElement('li');
+    let time = new Date();
     item.innerHTML = `
     			<p class="x">${time.toLocaleTimeString('en-US')}</p>
       			<p><b class="user"> ${username} </b>${msg}</p>
@@ -14,16 +19,24 @@ function makeMessageHTML(username, msg) {
     return item;
 }
 
+signin.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('cat');
+    username = user.value;
+    console.log(username);
+    signin.remove();
+});
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
-        socket.emit('chat message', input.value);
+        socket.emit('chat message', {username: username, message: input.value});
         input.value = '';
     }
 });
 
 socket.on('chat message', function(msg) {
-    var item = makeMessageHTML('fix', msg);
+    let item = makeMessageHTML(msg.username, msg.message);
     messages.appendChild(item);
     messages.scrollTo(0, messages.scrollHeight);
 });

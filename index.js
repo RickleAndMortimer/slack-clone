744f33a65b1f70c7a62aabeb0b9b48df27c26df5
nxya <nathacutlan@gmail.com>
2022-09-07 19:26:47 -0400

    a

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 const express = require('express');
+const fs = require('fs');                                                       
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-
-const options = { /* */ };
 const port  = 3000;
 
+const { MongoClient } = require("mongodb");
+const rawdata = fs.readFileSync('config.json');                                 
+const config = JSON.parse(rawdata);                                             
+                                                                                
+const uri =                                                                     
+  `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASS}@${config.MONGO_URL}/?retryWrites=true&w=majority`;
+                                                                                
+const client = new MongoClient(uri);                                            
+                                                                                
+
 app.use("", express.static(__dirname));
+app.use(express.json());
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/api/v1/users', async (req, res) => {
+    console.log(req.body);
+    try {
+        const database = client.db('slack-clone');                             
+        const users_collection = database.collection('users');                           
+
+        await users_collection.find({ username: req.body.username }).toArray(async function(err, result) {
+            if (err) throw err;
+            if (result.length == 0) {
+                const user = await users_collection.insertOne({ 'username': req.body.username });
+                res.send(user);
+            }
+        });
+    } finally {
+        console.log("End");
+    }
+});
+
+app.get('/api/v1/channels', async (req, res) => {
+    try {
+        const database = client.db('slack-clone');                             
+        const channels = database.collection('channels');                           
+        console.log(channels);
+
+        await channels.find({}).toArray(function(err, result) {
+            if (err) throw err;
+            res.send(result);
+        });
+    } finally {
+        console.log("End");
+    }
 });
 
 io.on('connection', (socket) => {
@@ -23,11 +66,13 @@ io.on('connection', (socket) => {
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', {username: msg.username, message: msg.message});
-  });
+	socket.on('chat message', (msg) => {
+        if (msg.username)
+            io.emit('chat message', {username: msg.username, message: msg.message});
+	});
 });
 
 server.listen(port, () => {
     console.log(`listening in ${port}`);
 });
+
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let socket = io();
 
+let content = document.getElementById('content');
+
 let messages = document.getElementById('messages');
+let sendButton = document.getElementById('send-button');
 let messagebar = document.getElementById('messagebar');
 let messagefield = document.getElementById('messagefield');
 
@@ -8,6 +11,11 @@ let user = document.getElementById('username');
 let signin = document.getElementById('signin');
 
 let username = '';
+
+content.style.background = 'white';
+content.style.filter = 'blur(8px)';
+messagefield.style.pointerEvents = 'none';
+sendButton.style.pointerEvents = 'none';
 
 function makeMessageHTML(username, msg) {
     let item = document.createElement('li');
@@ -21,15 +29,29 @@ function makeMessageHTML(username, msg) {
 
 signin.addEventListener('submit', e => {
     e.preventDefault();
-    console.log('cat');
     username = user.value;
-    console.log(username);
     signin.remove();
+    content.style.filter = 'blur(0px)';
+    messagefield.style.pointerEvents = 'auto';
+    sendButton.style.pointerEvents = 'auto';
+    fetch('http://localhost:3000/api/v1/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            'username': username
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        response.json()
+    }).then((data) => {
+        console.log(data);
+    });
 });
 
 messagebar.addEventListener('submit', function(e) {
     e.preventDefault();
-    if (messagefield.value) {
+    if (messagefield.value && username) {
         socket.emit('chat message', {username: username, message: messagefield.value});
         messagefield.value = '';
     }

1b7afe20abb7906047612ac102673741a559480d
nxya <nathacutlan@gmail.com>
2022-09-07 20:12:12 -0400

    frontend tweak

--- script.js
+++ script.js
@@ -17,11 +17,10 @@ content.style.filter = 'blur(8px)';
 messagefield.style.pointerEvents = 'none';
 sendButton.style.pointerEvents = 'none';
 
-function makeMessageHTML(username, msg) {
+function makeMessageHTML(username, msg, time) {
     let item = document.createElement('li');
-    let time = new Date();
     item.innerHTML = `
-    			<p class="x">${time.toLocaleTimeString('en-US')}</p>
+    			<p class="x">${(new Date(time)).toLocaleTimeString('en-US')}</p>
       			<p><b class="user"> ${username} </b>${msg}</p>
     			`;
     return item;
@@ -58,7 +57,7 @@ messagebar.addEventListener('submit', function(e) {
 });
 
 socket.on('chat message', function(msg) {
-    let item = makeMessageHTML(msg.username, msg.message);
+    let item = makeMessageHTML(msg.username, msg.message, msg.time);
     messages.appendChild(item);
     messages.scrollTo(0, messages.scrollHeight);
 });
